9177780a883dbf6c59c14fe3b737a880e6f00f1c
William Vasselin <william.vasselin@gmail.com>
2021-11-19 11:32:46 +0100

    update of FAQ
    FAQ full of questions

--- src/components/AppFAQ.vue
+++ src/components/AppFAQ.vue
@@ -6,27 +6,35 @@
 
         <div class="p-4 m-6 text-left">
             <div class="text-left text-gray-900 text-2xl font-bold mt-6">
-                Questions
+                1. Je souhaite annuler ma commande, est-ce possible ?
                 <hr style="height: 2px; width: 50%;">
             </div>
             <p>
-                Reponse Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                Oui, vous avez 12h à partir du moment où votre commande a été passée. Envoyez-nous un e-mail et nous annulerons votre commande et effectuerons le remboursement de total de la somme.
             </p>
 
             <div class="text-left text-gray-900 text-2xl font-bold mt-6">
-                Questions
+                2. Je ne suis pas satisfait de ma commande, puis-je être remboursé ?
                 <hr style="height: 2px; width: 50%;">
             </div>
             <p>
-                Reponse Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                Lorsque vous récuperez votre commande sur le point de retrait, VerreTech vous demande le niveau de satisfaction du produit. Il est donc impossible de rendre un produit fait sur mesure une fois la commande retirée.
             </p>
 
             <div class="text-left text-gray-900 text-2xl font-bold mt-6">
-                Questions
+                3. Comment faire pour vous contacter ?
                 <hr style="height: 2px; width: 50%;">
             </div>
             <p>
-                Reponse Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                Vous avez toutes les informations de contact depuis notre page "Contactez-nous".
+            </p>
+
+            <div class="text-left text-gray-900 text-2xl font-bold mt-6">
+                4. Comment le réception de la commande se passe t-elle ?
+                <hr style="height: 2px; width: 50%;">
+            </div>
+            <p>
+                Lorsque vous commandez votre produit sur notre boutique, il vous ai proposé à la fin de la commande, un lieu de retrait, la livraison à domicile n'est pas disponible, une fois votre commande passé et le point de retrait choisis, il vous ait donné une date et une heure à laquelle récupérer la commande passée précédemment. Il ne vous reste plus qu'à venir et repartir avec votre produit.
             </p>
         </div>
     </div>
