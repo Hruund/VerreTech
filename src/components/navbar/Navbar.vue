e66d65443815e7df9fa2d65fb6077f5598136e0c
William Vasselin <william.vasselin@gmail.com>
2021-11-19 12:13:29 +0100

    edit navbar
    ajout d'espaces

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -13,15 +13,15 @@
         </div>
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex-shrink-0 flex items-center">
-            <router-link to="/"><img class="hidden lg:block h-8 w-auto" src="https://i.imgur.com/imy5E5o.png" alt="Workflow" /></router-link>
+            <router-link to="/"><img class="mx-6 hidden lg:block h-8 w-auto" src="https://i.imgur.com/imy5E5o.png" alt="Workflow" /></router-link>
           </div>
           <div class="hidden sm:block sm:ml-6">
             <div class="flex space-x-4">
               <div class="relative mx-auto text-gray-600">
-                <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                <input class="mx-6 border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
                   type="search" name="search" placeholder="Rechercher un produit">
                 <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
-                  <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                  <svg class="text-gray-600 h-4 w-12 fill-current" xmlns="http://www.w3.org/2000/svg"
                     xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
                     viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
                     width="512px" height="512px">
