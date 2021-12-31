78bcd8b03ed5dc997dbe0ab5f67c6b4fe739d4c8
Ambre Chognot <a.chognot64@gmail.com>
2021-12-31 15:10:53 +0100

    Ajout second exemple de graph

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -8,6 +8,11 @@
     <div class="mt-5">
         <canvas id="retrait-chart"></canvas>
     </div>
+    
+    <h1>Nombre de clients sur l'année en cours</h1>
+    <div class="mt-5">
+        <canvas id="layanan-chart"></canvas>
+    </div>
 </template>
 
 <script>
@@ -32,6 +37,39 @@ export default {
     // Graphes de retrait
     const ctx2 = document.getElementById('retrait-chart');
     new Chart(ctx2, this.ChartData);
+
+    // Sinon tu peux aussi les créer comme ça en direct :
+    // https://stackoverflow.com/questions/53172262/multiple-charts-in-one-page-with-chart-js/53172498
+    var ctx3 = document.getElementById("layanan-chart").getContext('2d');
+    var data = {
+        datasets: [{
+            data: [10, 20, 30],
+            backgroundColor: [
+                '#3c8dbc',
+                '#f56954',
+                '#f39c12',
+            ],
+        }],
+        labels: [
+            'Request',
+            'Layanan',
+            'Problem'
+        ]
+    };
+    new Chart(ctx3, {
+        type: 'doughnut',
+        data: data,
+        options: {
+            responsive: false,
+            maintainAspectRatio: false,
+            legend: {
+                position: 'bottom',
+                labels: {
+                    boxWidth: 12
+                }
+            }
+        }
+    });
   }
 }
 </script>
