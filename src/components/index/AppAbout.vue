4f44195faddc0577809525ca34035d37a2809bff
Lucas Victorion <lucas.victorion@gmail.com>
2022-01-19 11:55:35 +0100

    Corr orthographe

--- src/components/index/AppAbout.vue
+++ src/components/index/AppAbout.vue
@@ -9,11 +9,11 @@
         <div id="home" class="mt-4">
             <div class="text-left p-2 m-4">
                 <p>
-                    Le groupe Verre-Tech est un groupe 100% familial créé en 1995 qui a étalie son siège social dans la ville de Besançon dans l'est de la France.
+                    Le groupe Verre-Tech est un groupe 100% familial créé en 1995 qui a établie son siège social dans la ville de Besançon, dans l'est de la France.
                 </p>
                 <p class="mt-4">
-                    Depuis son origine Verre-Tech est spécialisée et leader dans la fabrication et la distribution en magasins de produits en verre techniques et décoratifs.
-                    Petit à petit l'entreprise a sû se répandre dans différents secteurs en ouvrant d'autres entrepôts permettant à tous d'accèder à notre gamme de produits de verre.
+                    Depuis sa création, Verre-Tech est spécialisé et leader dans la fabrication et la distribution en magasins de produits en verre techniques et décoratifs.
+                    Petit à petit, l'entreprise a sû s'étendre dans différents secteurs en ouvrant d'autres entrepôts et boutiques, permettant à tous d'accèder à notre gamme de produits de verre.
                 </p>
             </div>
             <div @click="scrollToTop" class="cursor-pointer">
