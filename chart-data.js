ace71df3ca7377694210968cdf156c7ee6996a96
Lucasvct <lucas.victorion@gmail.com>
2022-05-18 10:56:50 +0200

    Modifs data stats admin

--- chart-data.js
+++ chart-data.js
@@ -1,18 +1,18 @@
 export const currentYearOrdersData = {
   type: "line",
   data: {
-    labels: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"],
+    labels: ["Janvier", "Février", "Mars", "Avril", "Mai"],
     datasets: [
       {
         label: "Nombre de commandes",
-        data: [2823, 2587, 3241, 3768, 2985, 2894, 3876, 4023, 3789, 3897, 5203, 4873],
+        data: [2823, 2587, 3241, 3768, 1245],
         backgroundColor: "rgba(54,73,93,.5)",
         borderColor: "#36495d",
         borderWidth: 3
       },
       {
         label: "Nombre de retraits en boutiques Verre-Tech",
-        data: [1267, 1234, 1423, 2164, 1693, 1407, 2109, 2389, 2485, 2465, 2801, 2623],
+        data: [1267, 1234, 1423, 2164, 829],
         backgroundColor: "rgba(71, 183,132,.5)",
         borderColor: "#47b784",
         borderWidth: 3
@@ -38,18 +38,18 @@ export const currentYearOrdersData = {
 export const currentMonthViewsData = {
   type: "line",
   data: {
-    labels: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"],
+    labels: ["Janvier", "Février", "Mars", "Avril", "Mai"],
     datasets: [
       {
         label: "Nombre de visites sur la boutique",
-        data: [98923, 107856, 120984, 121293, 113098, 111876, 119845, 137856, 149583, 169487, 178394, 172392],
+        data: [98923, 107856, 120984, 121293, 78454],
         backgroundColor: "rgba(54,73,93,.5)",
         borderColor: "#36495d",
         borderWidth: 3
       },
       {
         label: "Nombre de connexions utilisateur",
-        data: [13342, 14203, 19038, 18987, 14012, 12845, 11213, 16045, 19043, 21567, 23794, 24231],
+        data: [13342, 14203, 19038, 18987, 8241],
         backgroundColor: "rgba(71, 183,132,.5)",
         borderColor: "#47b784",
         borderWidth: 3
